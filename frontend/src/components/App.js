19bca69f74b243df7d7842dc630d6c3138612203
harish ashwin raj. s <ashwinharishraj@gmail.com>
2021-03-20 17:06:46 +0530

    first commit

---
+++ frontend/src/components/App.js
@@ -0,0 +1,16 @@
+import React,{Component} from "react";
+import ReactDOM from "react-dom";
+
+export default function App( Component ){
+
+    return(
+      <div>
+<div className ="container hell">
+<p> hell is finer than heaven</p>
+</div>
+      </div>
+    )
+
+}
+
+ReactDOM.render(<App/>,document.getElementById("app"));
